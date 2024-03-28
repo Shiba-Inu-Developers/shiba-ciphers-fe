fc748e3949940d90dc4f64af287214f098bc20c7
MildyS <ilovsky.ilovsky@gmail.com>
2024-03-28 14:26:15 +0100

    Načítanie údajov v profile (treba zmenit PUT aby vracal upravené údaje)

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,5 +1,5 @@
-import { Component, ViewChild, ViewContainerRef } from '@angular/core';
-import { Subject, pipe } from 'rxjs';
+import { Component, ViewChild, ViewContainerRef, AfterViewInit } from '@angular/core';
+import {Subject, pipe, Observable} from 'rxjs';
 import { ModalComponent } from 'src/app/components/modal/modal.component';
 import {
   NgbActiveModal,
@@ -8,24 +8,43 @@ import {
 } from '@ng-bootstrap/ng-bootstrap';
 import { ModalService } from 'src/app/components/modal/modal.service';
 import { takeUntil } from 'rxjs/operators';
+import {User} from "oidc-client";
+import {UserService} from "../../services/user.service";
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements AfterViewInit {
+
+  firstName = '';
+  lastName = '';
+  email = '';
+
   // private readonly ngUnsubscribe = new Subject();
   reset: Subject<boolean> = new Subject<boolean>();
   modalRef: NgbModalRef | undefined;
   constructor(
     private modalService: ModalService,
     private viewContainerRef: ViewContainerRef,
+    private userService: UserService,
     private ngModal: NgbModal
   ) {}
 
   ngOnInit() {
     console.log('ProfileComponent');
+  }
+
+  ngAfterViewInit(): void {
+    this.userService.getUser().subscribe({
+      next: (data) => {
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.email = data.email;
+      },
+      error: (error) => console.error('There was an error!', error)
+    });
   }
 
   onDeletePic(e: any): void {
@@ -45,4 +64,19 @@ export class ProfileComponent {
     });
   }
   onEditPic(e: any): void {}
+
+  onEditData(): void {
+    let userData = {
+      firstName: this.firstName,
+      lastName: this.lastName,
+    };
+    this.userService.putUser(userData).subscribe({
+      next: (data) => {
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+      },
+      error: (error) => console.error('There was an error!', error)
+    });
+  }
+
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,22 @@ export class UserService {
     this.http.get('/api/Users').subscribe((result) => {
       console.warn('result', result);
     });
+  }
+
+  getUser(): Observable<any> {
+    console.log('getUser1');
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    console.log('getUser2');
+    return this.http.get<any>('/api/user-info', { headers });
+  }
+
+  putUser(data: any): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${localStorage.getItem('token')}`
+    });
+    return this.http.put<any>('/api/update-user-info', data, { headers });
   }
 
 

934c23be79426064c27dd1c0314d27be3c5f0fe3
MildyS <ilovsky.ilovsky@gmail.com>
2024-04-03 23:19:34 +0200

    my documents (teraz aj fily)

---
+++ src/app/pages/my-documents/my-documents.component.ts
@@ -0,0 +1,24 @@
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-my-documents',
+  templateUrl: './my-documents.component.html',
+  styleUrls: ['./my-documents.component.css']
+})
+export class MyDocumentsComponent implements OnInit{
+
+  documents: any[] = [];
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getAllDocuments()
+  }
+
+  getAllDocuments() {
+    this.http.get<any[]>('/api/history-images').subscribe((documents: any[]) => {
+      this.documents = documents;
+    });
+  }
+}
